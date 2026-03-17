897870297de3060d42f755c5511439b6265d7b54
dangtuandat123 <datxtofficial@gmail.com>
2026-03-17 15:59:42 +0700

    feat: Add initial admin dashboard frontend build with new assets.

--- backend/public/assets/AdminSettingsPage-CBuz3uyI.js
+++ backend/public/assets/AdminSettingsPage-CBuz3uyI.js
@@ -1,4 +1,4 @@
-import{c as T,r as c,j as n,d as z,V as A,W as h,Y as G,e as D,h as P,i as B,Q as L,B as M,f as U,v as K,k as C,t as p,C as V,Z as w,m as g}from"./index-BJlehp-u.js";import{S as H}from"./switch-B9sYk-Yw.js";import{S as q}from"./save-Dlcq-KX_.js";/**
+import{c as T,r as c,j as n,d as z,V as A,W as h,Y as G,e as D,h as P,i as B,Q as L,B as M,f as U,v as K,k as C,t as p,C as V,Z as w,m as g}from"./index-BRzYNpWE.js";import{S as H}from"./switch-CvH2BOB1.js";import{S as q}from"./save-C-fT0f63.js";/**
  * @license lucide-react v0.575.0 - ISC
  *
  * This source code is licensed under the ISC license.
