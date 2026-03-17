19acf2ea6602474462121ee579f2b04cbfca46e8
dangtuandat123 <datxtofficial@gmail.com>
2026-03-17 07:32:13 +0700

    feat: Add new frontend build assets for the admin dashboard and various administrative pages.

--- backend/public/assets/save-BcjKDdHD.js
+++ backend/public/assets/save-BcjKDdHD.js
@@ -1,4 +1,4 @@
-import{c as a}from"./index-BgiFqIiB.js";/**
+import{c as a}from"./index-yjmHeq3F.js";/**
  * @license lucide-react v0.575.0 - ISC
  *
  * This source code is licensed under the ISC license.
