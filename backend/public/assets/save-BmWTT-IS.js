f72b76a1e5b8fbfbad1923807204ff07b54aa24b
dangtuandat123 <datxtofficial@gmail.com>
2026-03-17 16:04:32 +0700

    feat: Add compiled assets for the admin dashboard and related pages.

--- backend/public/assets/save-BmWTT-IS.js
+++ backend/public/assets/save-BmWTT-IS.js
@@ -1,4 +1,4 @@
-import{c as a}from"./index-BRzYNpWE.js";/**
+import{c as a}from"./index-Bykc_iF9.js";/**
  * @license lucide-react v0.575.0 - ISC
  *
  * This source code is licensed under the ISC license.
