d463b4969fa1ae3f8f521d1d8d92095f1a71f51a
dangtuandat123 <datxtofficial@gmail.com>
2026-03-31 08:12:45 +0700

    feat: implement admin management pages and landing page structure

--- backend/public/assets/AdminGeneratePage-Dv81GeMy.js
+++ backend/public/assets/AdminGeneratePage-Dv81GeMy.js
@@ -1,4 +1,4 @@
-import{c as z,r,j as e,d as G,B as L,e as m,h as x,i as p,Q as u,f as g,v as C,H as q,m as y,l as K,k as d,t as h,w as O,x as V,y as H,z as Q,A as $}from"./index-D64prTZI.js";import{S as M}from"./switch-9o41UD4R.js";import{S as W}from"./save-CGDGgKVu.js";/**
+import{c as z,r,j as e,d as G,B as L,e as m,h as x,i as p,Q as u,f as g,v as C,H as q,m as y,l as K,k as d,t as h,w as O,x as V,y as H,z as Q,A as $}from"./index-By78ulAN.js";import{S as M}from"./switch-AHkLIRxO.js";import{S as W}from"./save-Bl-IlyP_.js";/**
  * @license lucide-react v0.575.0 - ISC
  *
  * This source code is licensed under the ISC license.
--- backend/public/assets/AdminModelsPage-2KhnkFGK.js
+++ backend/public/assets/AdminModelsPage-2KhnkFGK.js
@@ -1,4 +1,4 @@
-import{c as z,r as n,j as e,B as i,P as D,d as I,e as E,f as O,l as B,F as K,T as P,D as F,q as L,s as A,u as H,v as c,m as o,H as J,E as R,k as d,t as l}from"./index-D64prTZI.js";import{S as X}from"./switch-9o41UD4R.js";/**
+import{c as z,r as n,j as e,B as i,P as D,d as I,e as E,f as O,l as B,F as K,T as P,D as F,q as L,s as A,u as H,v as c,m as o,H as J,E as R,k as d,t as l}from"./index-By78ulAN.js";import{S as X}from"./switch-AHkLIRxO.js";/**
  * @license lucide-react v0.575.0 - ISC
  *
  * This source code is licensed under the ISC license.
