1157c4dcc6ba69b738c13c8b2b7c0abd14d32ea6
dangtuandat123 <datxtofficial@gmail.com>
2026-04-05 11:25:03 +0700

    feat: add template management features and image upload components to admin dashboard

--- backend/public/assets/AdminSettingsPage-cKIhk95Q.js
+++ backend/public/assets/AdminSettingsPage-cKIhk95Q.js
@@ -1,4 +1,4 @@
-import{c as T,r as c,j as n,d as z,V as A,W as h,Y as G,e as D,h as P,i as B,Q as L,B as M,f as U,v as K,k as C,t as p,C as V,Z as w,m as g}from"./index-BOve2zeU.js";import{S as H}from"./switch-CXOuZOX2.js";import{S as q}from"./save-CvBRyQIE.js";/**
+import{c as T,r as c,j as n,d as z,V as A,W as h,Y as G,e as D,h as P,i as B,Q as L,B as M,f as U,v as K,k as C,t as p,C as V,Z as w,m as g}from"./index-CQOCM5Ez.js";import{S as H}from"./switch-DWw0_4L8.js";import{S as q}from"./save-DXSOnlXr.js";/**
  * @license lucide-react v0.575.0 - ISC
  *
  * This source code is licensed under the ISC license.
--- backend/public/assets/save-DXSOnlXr.js
+++ backend/public/assets/save-DXSOnlXr.js
@@ -1,4 +1,4 @@
-import{c as a}from"./index-BOve2zeU.js";/**
+import{c as a}from"./index-CQOCM5Ez.js";/**
  * @license lucide-react v0.575.0 - ISC
  *
  * This source code is licensed under the ISC license.
