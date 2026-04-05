2aedece6fbb96c06e74f59f62c7f22595cad28cc
dangtuandat123 <datxtofficial@gmail.com>
2026-04-05 15:03:48 +0700

    feat: add AIToolsPage component to display available and upcoming AI tools

--- backend/public/assets/AdminSettingsPage-oG6NEC79.js
+++ backend/public/assets/AdminSettingsPage-oG6NEC79.js
@@ -1,4 +1,4 @@
-import{c as T,r as c,j as n,d as z,V as A,W as h,Y as G,e as D,h as P,i as B,Q as L,B as M,f as U,v as K,k as C,t as p,C as V,Z as w,m as g}from"./index-DsGU1uLk.js";import{S as H}from"./switch-B7PGcWD1.js";import{S as q}from"./save-CZfXmJsg.js";/**
+import{c as T,r as c,j as n,d as z,V as A,W as h,Y as G,e as D,h as P,i as B,Q as L,B as M,f as U,v as K,k as C,t as p,C as V,Z as w,m as g}from"./index-DIAeD3m2.js";import{S as H}from"./switch-CpCips80.js";import{S as q}from"./save-BDOyMIpX.js";/**
  * @license lucide-react v0.575.0 - ISC
  *
  * This source code is licensed under the ISC license.
--- backend/public/assets/save-BDOyMIpX.js
+++ backend/public/assets/save-BDOyMIpX.js
@@ -1,4 +1,4 @@
-import{c as a}from"./index-DsGU1uLk.js";/**
+import{c as a}from"./index-DIAeD3m2.js";/**
  * @license lucide-react v0.575.0 - ISC
  *
  * This source code is licensed under the ISC license.
