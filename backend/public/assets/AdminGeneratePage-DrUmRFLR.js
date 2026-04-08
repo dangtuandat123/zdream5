61aa9a9767cb569afdd8277050145900f98c310a
dangtuandat123 <datxtofficial@gmail.com>
2026-04-08 18:12:19 +0700

    feat: implement image generation page with justified gallery and prompt history management

--- backend/public/assets/AdminGeneratePage-DrUmRFLR.js
+++ backend/public/assets/AdminGeneratePage-DrUmRFLR.js
@@ -1,4 +1,4 @@
-import{c as z,r,j as e,d as G,B as L,e as m,h as x,i as p,Q as u,f as g,v as C,H as q,m as y,l as K,k as d,t as h,w as O,x as V,y as H,z as Q,A as $}from"./index-IZVPDv2-.js";import{S as M}from"./switch-CQtv4s3q.js";import{S as W}from"./save-DMrUyAv-.js";/**
+import{c as z,r,j as e,d as G,B as L,e as m,h as x,i as p,Q as u,f as g,v as C,H as q,m as y,l as K,k as d,t as h,w as O,x as V,y as H,z as Q,A as $}from"./index-fKbVatGM.js";import{S as M}from"./switch-DnknGJDG.js";import{S as W}from"./save-FUQqdGwi.js";/**
  * @license lucide-react v0.575.0 - ISC
  *
  * This source code is licensed under the ISC license.
