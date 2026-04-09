946631ea602f9ad332826e4813a9c6993a8eeb02
dangtuandat123 <datxtofficial@gmail.com>
2026-04-09 13:43:19 +0700

    feat: implement admin management pages and generate page component

--- backend/public/assets/AdminGeneratePage-CAWjNDqV.js
+++ backend/public/assets/AdminGeneratePage-CAWjNDqV.js
@@ -1,4 +1,4 @@
-import{c as z,r,j as e,d as G,B as L,e as m,h as x,i as p,Q as u,f as g,v as C,H as q,m as y,l as K,k as d,t as h,w as O,x as V,y as H,z as Q,A as $}from"./index-tklZvs9l.js";import{S as M}from"./switch-DtK_3MRz.js";import{S as W}from"./save-rBFs-Rvo.js";/**
+import{c as z,r,j as e,d as G,B as L,e as m,h as x,i as p,Q as u,f as g,v as C,H as q,m as y,l as K,k as d,t as h,w as O,x as V,y as H,z as Q,A as $}from"./index-BL-qhz_P.js";import{S as M}from"./switch-lpzJq_hJ.js";import{S as W}from"./save-D8u_SJwe.js";/**
  * @license lucide-react v0.575.0 - ISC
  *
  * This source code is licensed under the ISC license.
