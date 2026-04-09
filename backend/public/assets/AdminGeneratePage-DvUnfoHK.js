ac621d309a9233d2c7e764ecf763588a326d526a
dangtuandat123 <datxtofficial@gmail.com>
2026-04-09 13:25:56 +0700

    feat: add AdminSettingsPage and integrate system settings management functionality

--- backend/public/assets/AdminGeneratePage-DvUnfoHK.js
+++ backend/public/assets/AdminGeneratePage-DvUnfoHK.js
@@ -1,4 +1,4 @@
-import{c as z,r,j as e,d as G,B as L,e as m,h as x,i as p,Q as u,f as g,v as C,H as q,m as y,l as K,k as d,t as h,w as O,x as V,y as H,z as Q,A as $}from"./index-CXIZyEI4.js";import{S as M}from"./switch-CWIokoJL.js";import{S as W}from"./save-DqJwrfv5.js";/**
+import{c as z,r,j as e,d as G,B as L,e as m,h as x,i as p,Q as u,f as g,v as C,H as q,m as y,l as K,k as d,t as h,w as O,x as V,y as H,z as Q,A as $}from"./index-CNn_uZzY.js";import{S as M}from"./switch-Blf3s3oc.js";import{S as W}from"./save-r3fios2F.js";/**
  * @license lucide-react v0.575.0 - ISC
  *
  * This source code is licensed under the ISC license.
